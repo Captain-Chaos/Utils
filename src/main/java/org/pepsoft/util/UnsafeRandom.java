74ba270b3c1890aa0e2a7b1a3c30ebb8e9452489
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2025-07-09 03:55:36 +1000

    random utils

--- src/main/java/org/pepsoft/util/UnsafeRandom.java
+++ src/main/java/org/pepsoft/util/UnsafeRandom.java
@@ -18,6 +18,10 @@ public class UnsafeRandom {
         return (int) ((this.next()&MSK)>>(48-bits));
     }
 
+    public float nextFloat() {
+        return next(24) * 0x1.0p-24f;
+    }
+
     public int nextInt(int bound) {
         int r = next(31);
         int m = bound - 1;
@@ -30,6 +34,22 @@ public class UnsafeRandom {
                 ;
         }
         return r;
+    }
+
+
+    public static int fastNextInt(long seed, int bound) {
+        seed = (seed^MUL)*MUL+ADD;
+        int m = bound - 1;
+        if ((bound & m) == 0)  // i.e., bound is a power of 2
+            return (int)((bound * ((seed&MSK)>>17)) >> 31);
+        else {
+            int r;
+            for (int u = (int) ((seed&MSK)>>17);
+                 u - (r = u % bound) + m < 0;
+                 u = (int) (((seed=seed*MUL+ADD)&MSK)>>17))
+                ;
+            return r;
+        }
     }
 
     private static final long MUL = 0x5DEECE66DL;
